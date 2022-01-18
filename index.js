1072dc65abdc335b2457a5e589e0e1f94e99b00e
Niraj Kumar <nirajkumar.123580@gmail.com>
2022-01-19 03:21:17 +0530

    2nd Step

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const db = require('./config/mongoose');
+
+//auth process ....session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
 //Reading through the Post request
 app.use(express.urlencoded());
 //Breaks cookies Data into smaller and according to our needs
@@ -13,6 +18,22 @@ app.use(express.static('./assets'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 //Entry Point
+
+//session cookie
+app.use(session({
+    name: 'Social',
+    secret: '**************'
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100);
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+//-----------------------------
+
 app.use('/', require('./routes'));
 
 app.listen(port, function(err) {
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const user_Controller = require('../controller/users_controller');
 router.get('/sign-in', user_Controller.signIn);
 router.get('/profile', user_Controller.profile);
 router.post('/create-account', user_Controller.create_account);
-router.post('/login', user_Controller.login);
+//use passport to auth
+router.post('/login',
+    passport.authenticate(
+        'local', { failureRedirect: '/users/sign-in' }
+    ), user_Controller.login);
 
 
 module.exports = router;
